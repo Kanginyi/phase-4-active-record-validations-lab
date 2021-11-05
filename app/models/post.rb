8fb7805944024e3fac48726d71352d59360b7abf
Eric Yi <erickanginyi@gmail.com>
2021-11-05 14:10:43 -0400

    commit tree a7e172ebc931fe3a7f338f53cde4599138be7467

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+
+    validate :more_clickbait
+
+    def more_clickbait
+        unless title.to_s.match?(/(Won't Believe|Secret|Top \d+|Guess)/i)
+            errors.add :title, "clickb8"
+        end
+    end
 end
+
